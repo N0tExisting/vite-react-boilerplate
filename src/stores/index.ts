ce006d64564baeae6511c2e2b7c958b63cb2e0c4
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2021-12-26 18:57:07 +0100

    Who Needs to undo anyway

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,30 +2,31 @@ import * as isBrowser from 'is-browser';
 import { createContext, useContext } from 'react';
 import { createStoreon } from 'storeon';
 import { customContext } from 'storeon/react';
-import { createHistory } from '@storeon/undo';
+//import { createHistory } from '@storeon/undo';
 import { persistState } from '@storeon/localstorage';
 import { crossTab } from '@storeon/crosstab';
 import { storeonDevtools } from 'storeon/devtools';
 import DarkMode from './darkMode';
 
-const { module: undo, UNDO, REDO } = createHistory([]);
+//const { module: undo, UNDO, REDO } = createHistory([]);
 
-export const store = createStoreon([
-	DarkMode,
-	undo,
-	persistState(['darkMode']),
-	isBrowser &&
-		crossTab({
-			filter: (type) => {
-				const name = type.toString();
-				return name.startsWith('darkMode/');
-			},
-		}),
-	import.meta.env.DEV && storeonDevtools(),
-]);
+export const store = createStoreon(
+	[
+		DarkMode,
+		//undo,
+		persistState(['darkMode']),
+		isBrowser &&
+			crossTab({
+				filter: (type) => {
+					const name = type.toString();
+					return name.startsWith('darkMode/');
+				},
+			}),
+	].concat(import.meta.env.DEV ? storeonDevtools() : []),
+);
 
-export const Undo = () => store.dispatch(UNDO.valueOf());
-export const Redo = () => store.dispatch(REDO.valueOf());
+//export const Undo = () => store.dispatch(UNDO.valueOf());
+//export const Redo = () => store.dispatch(REDO.valueOf());
 
 export const StoreContext = createContext(store);
 export const StoreProvider = StoreContext.Provider;
