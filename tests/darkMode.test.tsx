bc86695c2f2882c270eb927901ee6130db311d17
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2021-12-26 18:11:06 +0100

    Add test (Akryum/peeky#58)

---
+++ tests/darkMode.test.tsx
@@ -0,0 +1,43 @@
+//import DarkMode from '$comp/DarkMode';
+import { createStoreon } from 'storeon';
+import DarkStore from '$store/darkMode';
+
+describe('Dark Mode', () => {
+	describe('Toggle', () => {
+		// TODO
+	});
+	describe('Store', () => {
+		let store = createStoreon([DarkStore]);
+		beforeEach(() => {
+			store = createStoreon([DarkStore]);
+		});
+
+		it('should match window media preference', () => {
+			const isLight = window.matchMedia(
+				'(prefers-color-scheme: light)',
+			).matches;
+			const mode = isLight ? 'light' : 'dark';
+			expect(store.get().darkMode === mode).toBe(true);
+		});
+		it('should be able to toggle', () => {
+			const original = store.get().darkMode;
+			const next = original === 'light' ? 'dark' : 'light';
+
+			store.dispatch('darkMode/toggle');
+			expect(store.get().darkMode).toBe(next);
+
+			store.dispatch('darkMode/toggle');
+			expect(store.get().darkMode).toBe(original);
+		});
+		describe('can be set', () => {
+			it('to dark mode', () => {
+				store.dispatch('darkMode/set', 'dark');
+				expect(store.get().darkMode).toBe('dark');
+			});
+			it('to light mode', () => {
+				store.dispatch('darkMode/set', 'light');
+				expect(store.get().darkMode).toBe('light');
+			});
+		});
+	});
+});
