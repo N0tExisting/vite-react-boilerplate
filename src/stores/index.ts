6a0fdd846adf5fd814cd6760c651541885ba4a7f
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2021-12-26 18:24:40 +0100

    Add Undo to store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,14 +2,17 @@ import * as isBrowser from 'is-browser';
 import { createContext, useContext } from 'react';
 import { createStoreon } from 'storeon';
 import { customContext } from 'storeon/react';
-//import { undoable, UNDO, REDO } from '@storeon/undo/full';
+import { createHistory } from '@storeon/undo';
 import { persistState } from '@storeon/localstorage';
 import { crossTab } from '@storeon/crosstab';
 import { storeonDevtools } from 'storeon/devtools';
 import DarkMode from './darkMode';
 
+const { module: undo, UNDO, REDO } = createHistory([]);
+
 export const store = createStoreon([
 	DarkMode,
+	undo,
 	persistState(['darkMode']),
 	isBrowser &&
 		crossTab({
@@ -20,6 +23,9 @@ export const store = createStoreon([
 		}),
 	import.meta.env.DEV && storeonDevtools(),
 ]);
+
+export const Undo = () => store.dispatch(UNDO.valueOf());
+export const Redo = () => store.dispatch(REDO.valueOf());
 
 export const StoreContext = createContext(store);
 export const StoreProvider = StoreContext.Provider;
