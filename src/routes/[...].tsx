686ef186167482ba8476c9a366dec1cd3703c29c
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2021-12-26 19:24:41 +0100

    Don't index the 404 page.

--- src/routes/[...].tsx
+++ src/routes/[...].tsx
@@ -1,9 +1,14 @@
 import type { VFC } from 'react';
 import { Link as A } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 
 const NotFound: VFC = () => {
 	return (
 		<div className="text-center">
+			<Helmet>
+				<meta name="robots" content="noindex follow" />
+				<title>404 - Not Found</title>
+			</Helmet>
 			<main className="flex flex-col items-center justify-center h-screen pb-1 text-xl dark:(text-white bg-gray-700) bg-white text-gray-700 h-min-full">
 				<h1 className="text-9xl">404</h1>
 				<h2 className="py-2 text-6xl">Page not found</h2>
